9c19e08726aa61c1b159e58a9e145384e67914c1
Nayadeth <na.probostediaz@gmail.com>
2025-01-27 00:16:01 -0300

    avance

---
+++ tipos-avanzados/01-listas.py
@@ -0,0 +1,10 @@
+numeros = [1, 2, 3]
+letras = ["a", "b", "c"]
+palabras = ["chanchito", "feliz"]
+palabrasFelices = ["chanchito", "feliz", "felipe", "alumnos"]
+booleans = [True, False, True, False]
+matriz = [[0, 1], [1, 0]]
+ceros = [0]*10
+alfanumericos = numeros + letras
+rango = list(range(1, 11))
+chars = list("hola mundo")
