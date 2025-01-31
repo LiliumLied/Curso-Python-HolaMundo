e5a53916cc2d9282532d7f69b5345804d8b11e07
Nayadeth <na.probostediaz@gmail.com>
2025-01-30 23:22:47 -0300

    avance

--- tipos-avanzados/12-operador-desempaquetar.py
+++ tipos-avanzados/12-operador-desempaquetar.py
@@ -1,3 +1,15 @@
-lista = [1, 2, 3, 4]
-print(1, 2, 3, 4)
-print(*lista)
+# lista1 = [1, 2, 3, 4]
+# # print(1, 2, 3, 4)
+# # print(*lista)
+
+# lista2 = [5, 6]
+
+# combinada = [*lista1, *lista2]
+# print(combinada)
+
+punto1 = {"x": 19}
+punto2 = {"x": 15}
+
+
+nuevoPunto = {**punto1, **punto2}
+print(nuevoPunto)
---
+++ tipos-avanzados/15-ejercicios.py
@@ -0,0 +1,18 @@
+string = "Hola mundo este es mi string"
+
+
+def quita_espacios(texto):
+    return [char for char in texto if char != " "]
+
+
+def cuenta_caracteres(lista):
+    chars_dict = {}
+    for char in lista:
+        if char in chars_dict:
+            chars_dict[char] += 1
+        else:
+            char
+
+
+sin_espacios = quita_espacios(string)
+print(sin_espacios)
