f169f9fb241ee31a725f46ce48921ca5e180ca4f
Nayadeth <na.probostediaz@gmail.com>
2025-02-02 21:28:57 -0300

    avance

--- tipos-avanzados/15-ejercicios.py
+++ tipos-avanzados/15-ejercicios.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 string = "Hola mundo este es mi string"
 
 
@@ -11,8 +12,38 @@ def cuenta_caracteres(lista):
         if char in chars_dict:
             chars_dict[char] += 1
         else:
-            char
+            chars_dict[char] = 1
+    return chars_dict
+
+
+def ordena(dict):
+    return sorted(
+        dict.items(),
+        key=lambda key: key[1],
+        reverse=True,
+    )
+
+
+def mayores_tuplas(lista):
+    maximo = lista[0][1]
+    respuesta = {}
+    for orden in lista:
+        if maximo > orden[1]:
+            break
+        respuesta[orden[0]] = orden[1]
+    return respuesta
+
+
+def crea_mensaje(diccionario):
+    mensaje = "Los que mas se repiten son: \n"
+    for key, valor in diccionario.items():
+        mensaje += f"- {key} con {valor} repeticiones \n"
+    return mensaje
 
 
 sin_espacios = quita_espacios(string)
-print(sin_espacios)
+contados = cuenta_caracteres(sin_espacios)
+ordenados = ordena(contados)
+mayores = mayores_tuplas(ordenados)
+mensaje = crea_mensaje(mayores)
+print(mensaje)
