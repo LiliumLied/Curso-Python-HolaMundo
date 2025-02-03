c87c3c58c2877ef0891a31861f79e309e4570492
Nayadeth <na.probostediaz@gmail.com>
2025-02-02 21:33:50 -0300

    avance

---
+++ tipos-avanzados/17-ejercicios-promocion.py
@@ -0,0 +1,15 @@
+def aplicar_promocion(compras):
+    clientes_con_promocion = []
+    for cliente, monto in compras.items():
+        if monto >= 150:
+            clientes_con_promocion.append(cliente)
+    return clientes_con_promocion
+
+
+compras = {
+    'Cliente1': 200,
+    'Cliente2': 100,
+    'Cliente3': 180}
+
+clientes_promocion = aplicar_promocion(compras)
+print(clientes_promocion)  # ['Cliente1', 'Cliente3']Copyright 2024holamundo.io
